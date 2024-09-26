6f4d669c0ad8fba1c1d65359984a0098acdaff28
patlim <impatlim@gmail.com>
2024-09-26 10:59:27 +0200

    fix lists in textSection

--- frontend/components/textSection.jsx
+++ frontend/components/textSection.jsx
@@ -3,11 +3,19 @@ import styled from 'styled-components';
 
 const customComponents = {
   block: {
-    normal: ({ children }) => <p data-scroll-fade="true">{children}</p>,
-    h1: ({ children }) => <h1 data-scroll-fade="true">{children}</h1>,
-    h2: ({ children }) => <h2 data-scroll-fade="true">{children}</h2>,
-    h3: ({ children }) => <h3 data-scroll-fade="true">{children}</h3>,
-    blockquote: ({ children }) => <blockquote data-scroll-fade="true">{children}</blockquote>,
+    normal: ({ children }) => <p data-scroll-fade>{children}</p>,
+    h1: ({ children }) => <h1 data-scroll-fade>{children}</h1>,
+    h2: ({ children }) => <h2 data-scroll-fade>{children}</h2>,
+    h3: ({ children }) => <h3 data-scroll-fade>{children}</h3>,
+    blockquote: ({ children }) => <blockquote data-scroll-fade>{children}</blockquote>,
+  },
+  list: {
+    bullet: ({ children }) => <ul data-scroll-fade>{children}</ul>,
+    number: ({ children }) => <ol data-scroll-fade>{children}</ol>,
+  },
+  listItem: {
+    bullet: ({ children }) => <li data-scroll-fade>{children}</li>,
+    number: ({ children }) => <li data-scroll-fade>{children}</li>,
   },
 };
 
@@ -18,6 +26,14 @@ const BodyContainer = styled.div`
 
   p:not(:last-child) {
     margin-bottom: 1.5rem;
+  }
+
+  ul {
+    text-align: left;
+    margin: 0 64px 1.5rem 64px;
+    @media screen and (max-width: 768px) {
+      margin: 0 12px 1.5rem 12px;
+    }
   }
 `;
 
