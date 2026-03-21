6a75e584bbe29314325b65804b69d0526479eaf4
patlim <125264372+patlim@users.noreply.github.com>
2026-03-22 10:59:40 +1100

    empty commit

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'; 
 import HomeSection from '../components/homeSection';
 import PageBuilder from '../components/pageBuilder';
 import client from '../client';
